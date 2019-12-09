6a80840b314ece20eb27b937068c5d2a177fb5a5
Willem van Klinken <willem.v.klinken@gmail.com>
2019-12-09 09:43:08 +0100

    day 7 : part 1 better

--- advent-of-code-clj/src/advent_of_code/day7.clj
+++ advent-of-code-clj/src/advent_of_code/day7.clj
@@ -1,18 +1,9 @@
 (ns advent-of-code.day7
-  (:require [advent-of-code.day5 :as intcode]))
+  (:require [advent-of-code.day5 :as intcode])
+  (refer advent-of-code.day5 :only [run-intcode]))
 
-; (comment
-;  (let [result 0]
-;    (doseq [w (vec (range 0 5))
-;            x (vec (range 0 5))
-;            y (vec (range 0 5))
-;            z (vec (range 0 5))]
-;      (let [output 0] ; irl to be output of running combo on intcode prog
-;         (if (> output result)
-;           (println result))))))
-
-(def input [
-            4,8,1001,8,10,8,105,1,1,0,21,42,59,76,85,106,187,268,349,430,99999,3,9,102,3,9,9,1001,9,2,9,1002,9,3,9,1001,9,3,9,4,9,99,3,9,102,3,9,9,101,3,9,9,1002,9,2,9,4,9,99,3,9,102,3,9,9,1001,9,4,9,1002,9,5,9,4,9,99,3,9,102,2,9,9,4,9,99,3,9,101,3,9,9,1002,9,2,9,1001,9,4,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,99])
+(def puz-input [
+                3,8,1001,8,10,8,105,1,0,0,21,42,59,76,85,106,187,268,349,430,99999,3,9,102,3,9,9,1001,9,2,9,1002,9,3,9,1001,9,3,9,4,9,99,3,9,102,3,9,9,101,3,9,9,1002,9,2,9,4,9,99,3,9,102,3,9,9,1001,9,4,9,1002,9,5,9,4,9,99,3,9,102,2,9,9,4,9,99,3,9,101,3,9,9,1002,9,2,9,1001,9,4,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,99])
 
 (defn run-amp
   "take setting and input - run intcode - output res"
@@ -34,9 +25,65 @@
         res-4 (run-amp res-3 2)]
    (clojure.pprint/pprint  "x")
    (println res-4)))
+(defn run-thru-amps
+  [input settings]
+  (let [[phase-1 phase-2 phase-3 phase-4 phase-5] (take 5 settings)
+        result (run-amp input [phase-1 0])
+        res-1 (run-amp (first result) [phase-2 (last (second result))])
+        res-2 (run-amp (first res-1) [phase-3 (last (second res-1))])
+        res-3 (run-amp (first res-2) [phase-4 (last (second res-2))])
+        res-4 (run-amp (first res-3) [phase-5 (last (second res-3))])]
+    (first (last res-4))))
+
+(defn get-phases
+  [a b]
+  (let [all (for [v (range a b)
+                  w (range a b)
+                  x (range a b)
+                  y (range a b)
+                  z (range a b)]
+              [v w x y z])
+
+        valid (reduce (fn [acc v]
+                        (if (= 5 (count (distinct v)))
+                          (conj acc v)
+                          acc))
+                      []
+                      all)]
+    valid))
+
+
+(defn aco
+  "amplifier controller"
+  [input]
+  (let [phases (get-phases 0 5)
+        outputs (reduce (fn [acc v]
+                           (conj acc (run-thru-amps input v)))
+                        []
+                        phases)]
+    (first (last outputs))))
 
 
 (defn -main
   [& args]
-  (let [x (advent-of-code.day5/run-intcode [0001 1 1 0 99])]
-    (println x)))
+  (println (str "prob 1: " (aco puz-input)))
+  (println (time (aco puz-input))))
+
+
+; ==============================
+; Future
+; A Future is similar to a Promise. The main difference is that Futures will evaluate an expression for you in another Thread. You don't have to set the thread up. Here's how the same thing works using future.
+;
+; (def the-answer (future
+;                    ;; do a lot of work in a new thread
+;                    ;; ...
+;                    ;; then deliver the answer
+;                    ;; the value of the last expression is delivered
+;                    42))
+;
+; ;; in the main thread, do some work
+; ;; ...
+; ;; then get the answer (which blocks until the answer is done)
+; (println (deref the-answer))))
+
+; One thing that trips up people new to Futures is that they swallow exceptions. If the code you run in the Future throws an exception, you won't hear about it until you deref it. When you deref it, the exception will be thrown again in the current thread.
